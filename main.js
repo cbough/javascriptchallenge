f85d8d398fb37d23283d62427dc884a1d5083b4f
Christopher Bough <christopherbough85@gmail.com>
2019-05-07 12:31:19 -0400

    Java HW number 2

--- main.js
+++ main.js
@@ -1,29 +1,12 @@
-var prices = [33, 26, 99, 120, 12, 45];
-var sum = 0;
-for(var i=0; i < prices.length; i++){
-
-    sum += parseInt(prices[i]);
-
+function reverse(str){
+  return str.split("").reverse().join("");
 }
-console.log("The sum is: ", sum);
 
-let j = 99;
+reverse('Marines')
 
-while (j > 0) {
-  if (j > 2) {
-    console.log(j + " crows on the wall. " + j + " crows.");
-    console.log("1 fell down and became a wight.");
-    console.log(j-1 + " crows on the wall");
-    j--;
-  }else if (j === 2){
-    console.log(j + " crows on the wall. " + j + " crows.");
-    console.log("1 fell down and became a wight.");
-    console.log(j-1 + " crow on the wall");
-    j--;
-  }else {
-    console.log("1 crow on the wall. 1 single crow.");
-    console.log("It fell down and became a wight.");
-    console.log("There's no one left to defend Westeros now.");
-    j--;
-  }
+for (var i=1; i < 101; i++){
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
 }
